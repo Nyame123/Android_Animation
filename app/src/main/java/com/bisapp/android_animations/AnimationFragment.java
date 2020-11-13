c4f96969b40737b381c6263eea6323bab1ae7b14
bnyame01 <bismark.nyame@ecomtrading.com>
2020-11-13 21:46:41 +0000

    animator set from xml

--- app/src/main/java/com/bisapp/android_animations/AnimationFragment.java
+++ app/src/main/java/com/bisapp/android_animations/AnimationFragment.java
@@ -1,28 +1,33 @@
 package com.bisapp.android_animations;
 
+import android.animation.Animator;
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
 import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.AnticipateInterpolator;
+import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.AnticipateOvershootInterpolator;
+import android.view.animation.LinearInterpolator;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
 
 public class AnimationFragment extends Fragment implements View.OnClickListener {
 
     private Button rotateBtn;
     private Button scaleBtn;
-    private Button moveBtn,fadeBtn;
+    private Button moveBtn, moveBtnX, playTogether, fadeBtn;
     private TextView firstTextview;
     private ObjectAnimator objectAnimator;
     private boolean isAnimationStarted;
+    private AnimatorSet set;
 
     @Override
     public View onCreateView(
@@ -37,11 +42,15 @@ public class AnimationFragment extends Fragment implements View.OnClickListener 
         super.onViewCreated(view, savedInstanceState);
         firstTextview = view.findViewById(R.id.textview_first);
         moveBtn = view.findViewById(R.id.moveBtn);
+        moveBtnX = view.findViewById(R.id.moveBtnX);
+        playTogether = view.findViewById(R.id.cominedAnim);
         scaleBtn = view.findViewById(R.id.scaleBtn);
         fadeBtn = view.findViewById(R.id.fadeBtn);
         rotateBtn = view.findViewById(R.id.rotateBtn);
 
         moveBtn.setOnClickListener(this);
+        moveBtnX.setOnClickListener(this);
+        playTogether.setOnClickListener(this);
         scaleBtn.setOnClickListener(this);
         rotateBtn.setOnClickListener(this);
         fadeBtn.setOnClickListener(this);
@@ -49,9 +58,9 @@ public class AnimationFragment extends Fragment implements View.OnClickListener 
 
     }
 
-    private void moveTextview(){
+    private void moveTextviewOnYaxis() {
         isAnimationStarted = true;
-        objectAnimator = ObjectAnimator.ofFloat(firstTextview, "translationY", 0,600);
+        objectAnimator = ObjectAnimator.ofFloat(firstTextview, "translationY", 0, 600);
         objectAnimator.setDuration(2000)
                 .setInterpolator(new AnticipateOvershootInterpolator());
         objectAnimator.setRepeatMode(ValueAnimator.REVERSE);
@@ -60,17 +69,150 @@ public class AnimationFragment extends Fragment implements View.OnClickListener 
 
     }
 
+    private void moveTextviewOnXaxis() {
+        isAnimationStarted = true;
+        objectAnimator = ObjectAnimator.ofFloat(firstTextview, "translationX", 0, 600);
+        objectAnimator.setDuration(2000)
+                .setInterpolator(new AccelerateDecelerateInterpolator());
+        objectAnimator.setRepeatMode(ValueAnimator.REVERSE);
+        objectAnimator.setRepeatCount(1);
+        objectAnimator.start();
+
+    }
+
+    private void rotateTextView() {
+        isAnimationStarted = true;
+        objectAnimator = ObjectAnimator.ofFloat(firstTextview,
+                "rotation", 0, 90);
+        objectAnimator.setDuration(2000)
+                .setInterpolator(new LinearInterpolator());
+        objectAnimator.setRepeatMode(ValueAnimator.REVERSE);
+        objectAnimator.setRepeatCount(1);
+        objectAnimator.start();
+
+    }
+
+    private void fadeTextview() {
+        isAnimationStarted = true;
+        objectAnimator = ObjectAnimator.ofFloat(firstTextview,
+                "alpha", 0, 1);
+        objectAnimator.setDuration(2000)
+                .setInterpolator(new LinearInterpolator());
+        objectAnimator.setRepeatMode(ValueAnimator.REVERSE);
+        objectAnimator.setRepeatCount(1);
+        objectAnimator.start();
+
+    }
+
+    private void scaleTextview() {
+        isAnimationStarted = true;
+        objectAnimator = ObjectAnimator.ofFloat(firstTextview,
+                "scaleY", 1, 20);
+        objectAnimator.setDuration(2000)
+                .setInterpolator(new LinearInterpolator());
+        objectAnimator.setRepeatMode(ValueAnimator.REVERSE);
+        objectAnimator.setRepeatCount(1);
+        objectAnimator.start();
+
+    }
+
+    private void animateWithViewAnimation(){
+        firstTextview.animate().scaleXBy(20).start();
+    }
+
+    private void combineAnimations() {
+        isAnimationStarted = true;
+        ObjectAnimator animX = ObjectAnimator.ofFloat(firstTextview,
+                "x", 50f);
+        animX.setDuration(2000)
+                .setInterpolator(new LinearInterpolator());
+
+        ObjectAnimator animY = ObjectAnimator.ofFloat(firstTextview,
+                "y", 100f);
+        animY.setDuration(2000)
+                .setInterpolator(new LinearInterpolator());
+
+        ObjectAnimator scaleAnim = ObjectAnimator.ofFloat(firstTextview,
+                "scaleY", 1, 20);
+        scaleAnim.setDuration(2000)
+                .setInterpolator(new LinearInterpolator());
+
+
+        set = new AnimatorSet();
+        set.playTogether(scaleAnim, animX, animY);
+        set.start();
+    }
+
+    private void loadAnimatorFromXml(){
+        AnimatorSet animatorInflater = (AnimatorSet) AnimatorInflater.loadAnimator(getContext(),R.animator.pivot_scale_animator);
+        animatorInflater.setTarget(firstTextview);
+        animatorInflater.start();
+    }
+
+    private void usingPropertyValueHolder() {
+        isAnimationStarted = true;
+        PropertyValuesHolder animX = PropertyValuesHolder.ofFloat("x", 50f);
+        PropertyValuesHolder animY = PropertyValuesHolder.ofFloat("y", 100f);
+        PropertyValuesHolder scaleY = PropertyValuesHolder.ofFloat("scaleY", 20);
+
+        ObjectAnimator.ofPropertyValuesHolder(firstTextview,animX, animY, scaleY).start();
+
+    }
+
     @Override
     public void onClick(View v) {
         int id = v.getId();
-        switch (id){
+        switch (id) {
             case R.id.moveBtn:
-                if (isAnimationStarted && objectAnimator != null){
+                if (isAnimationStarted && objectAnimator != null) {
                     objectAnimator.cancel();
                     isAnimationStarted = false;
-                }else {
-                    moveTextview();
+                } else {
+                    moveTextviewOnYaxis();
                 }
+                break;
+            case R.id.moveBtnX:
+                if (isAnimationStarted && objectAnimator != null) {
+                    objectAnimator.cancel();
+                    isAnimationStarted = false;
+                } else {
+                    moveTextviewOnXaxis();
+                }
+                break;
+            case R.id.rotateBtn:
+                if (isAnimationStarted && objectAnimator != null) {
+                    objectAnimator.cancel();
+                    isAnimationStarted = false;
+                } else {
+                    rotateTextView();
+                }
+                break;
+            case R.id.fadeBtn:
+                if (isAnimationStarted && objectAnimator != null) {
+                    objectAnimator.cancel();
+                    isAnimationStarted = false;
+                } else {
+                    fadeTextview();
+                }
+                break;
+            case R.id.scaleBtn:
+                if (isAnimationStarted && objectAnimator != null) {
+                    objectAnimator.cancel();
+                    isAnimationStarted = false;
+                } else {
+                    scaleTextview();
+                }
+                break;
+            case R.id.cominedAnim:
+                /*if (isAnimationStarted && set != null) {
+                    set.cancel();
+                    isAnimationStarted = false;
+                } else {
+                    combineAnimations();
+                }*/
+                //usingPropertyValueHolder();
+                loadAnimatorFromXml();
+
                 break;
         }
 
